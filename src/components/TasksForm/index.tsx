eb8fa6ff8b5d432a6dd9dc32d2da3b0d672ac2ab
XomekX <z_v_m_x@mail.ru>
2022-05-13 20:26:49 +0700

    styled the TasksForm component

--- src/components/TasksForm/index.tsx
+++ src/components/TasksForm/index.tsx
@@ -1,5 +1,6 @@
 import { FC, HTMLAttributes } from "react";
 import { stylesFilterAndJoin } from "../../misc/stylesSortAndJoin";
+import { Button, Form, MyInput } from "../UI";
 import styles from "./TasksForm.module.scss";
 
 interface ITasksFormProps extends HTMLAttributes<HTMLDivElement> {}
@@ -8,9 +9,11 @@ const TasksForm: FC<ITasksFormProps> = ({ className }) => {
   const TasksFormStyles = stylesFilterAndJoin([styles.tasksForm, className]);
 
   return (
-    <div className={TasksFormStyles}>
-      <div className="container"></div>
-    </div>
+    <Form className={TasksFormStyles}>
+      <MyInput className={styles.input} placeholder="Заголовок" />
+      <MyInput className={styles.input} placeholder="Описание" />
+      <Button className={styles.button}>Добавить</Button>
+    </Form>
   );
 };
 
